bb755c192d5795ea9356cce5f465085712b9eabb
JahidHasan-00 <jahid.hasan50001@gmail.com>
2025-02-19 23:56:53 +0600

    Created some methods where user can access necessary data and can send mail and sanitize message

--- class.js
+++ class.js
@@ -30,20 +30,36 @@ class Library {
 
     //constructor function:
     constructor(name, email) {
-        this.name = name
-        this.email = email
+        this._name = name
+        this._email = email
     }
 
-    //changeName Method:
-    changeName(name) {
-        this.name = name
+    //getName Method:
+    getName() {
+        return this._name
+    }
+    //setName Method:
+    setName(name) {
+        this._name = name
+    }
+    
+    //getEmail Method:
+    getEmail() {
+        return this._email
+    }
+    //setEmail Method:
+    setEmail(email) {
+        this._email = email        
     }
 
-    //send email Method:
-    sendEmail(msg) {
-        console.log('To', this.email)
-        console.log('Sending Email')
-        console.log('msg', msg)
+    //sendMail Method:
+    sendMail(msg) {
+        console.log('To:', this._email)
+        console.log('msg:', this._sanitizeMsg(msg))
+    }
+    //sanitizeMsg Method:
+    _sanitizeMsg(msg) {
+        return msg.trim().toLowerCase()
     }
 
     //Print Method:
@@ -55,9 +71,9 @@ class Library {
 const lib1 = new Library('knowledge House', 'knowledgehouse@gmail.com')
 const lib2 = new Library('Candle Library', 'candle@gmail.com')
 
-lib1.print()
-lib2.print()
+lib1.setName('Future Lib')
+lib1.setEmail('future@gmail.com')
 
-//change name of lib1
-lib1.changeName('Future knowledge House')
-lib1.print()
+console.log(lib1.getName(), lib1.getEmail())
+
+lib1.sendMail('     WOW! It is Working  ')
